d80e02381d1491eb54b758bfccac00d904261390
Nicholas <nicholasjoven@gmail.com>
2022-07-28 11:27:44 -0400

    switching branch to fix issue

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
-function PokemonPage() {
+// Create an index displaying Pokemon 'cards'
+// Render each Pokemon name, sprite, and hp in a card
+// When clicked, the card should toggle between displaying the front and back sprites
+// Allow users to search a Pokemon by its name in order to narrow down the cards shown
+// on the page
+// Wire up the form to add a missing Pokemon(Bulbasaur is missing, and you can probably 
+// intuit the image links to use based on the data you have).Since there aren't any validations, you may have to manually remove additions from the db.json file if you make a mistake on a POST request, etc. When a new Pokemon is added, it should show on the page without having to refresh.
+
+const PokemonPage = () => {
+
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      let req = await fetch('http://localhost:3001/pokemon')
+      let res = await req.json()
+      console.log('pokemonList', res)
+      setPokemonList(res)
+    }
+    fetchPokemon()
+  }, [])
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
